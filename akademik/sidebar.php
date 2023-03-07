7038a50be56860ffb51bf3fcfc0c4935556b0a33
incyberz <incyberz@yahoo.com>
2023-03-07 07:08:14 +0700

    update pagi

--- akademik/sidebar.php
+++ akademik/sidebar.php
@@ -6,13 +6,14 @@
       //   if(isset($menu[$i]) and $menu[$i][0]==1)echo "<li><a href='?".$menu[$i][1]."' ".$menu[$i][5]."><i class='icon_".$menu[$i][4]."'></i><span>".$menu[$i][3]."</span></a></li>";
       // }
       ?>
-      <li><a href="?"><i class="icon_easel_alt"></i>Dashboard</a></li>
-      <li><a href="?master"><i class="icon_genius"></i>Master Data</a></li>
-      <li><a href="?master&p=kurikulum"><i class="icon_genius"></i>Manage Kurikulum</a></li>
-      <li><a href="?manage_jadwal"><i class="icon_genius"></i>Manage Jadwal</a></li>
-      <li><a href="?manage_kelas"><i class="icon_genius"></i>Manage Kelas</a></li>
-      <li><a href="?manage_sesi"><i class="icon_genius"></i>Manage Sesi</a></li>
-      <li><a href="../tmp/relasi.png" target="_blank"><i class="icon_genius"></i>Relasi DB</a></li>
+      <li class=proper><a href="?"><i class="icon_easel_alt"></i>Dashboard</a></li>
+      <li class=proper><a href="?master"><i class="icon_genius"></i>Master Data</a></li>
+      <li class=proper><a href="?master&p=kurikulum"><i class="icon_genius"></i>Manage Kurikulum</a></li>
+      <li class=proper><a href="?manage_jadwal"><i class="icon_genius"></i>Manage Jadwal</a></li>
+      <li class=proper><a href="?manage_kelas"><i class="icon_genius"></i>Manage Kelas</a></li>
+      <li class=proper><a href="?manage_sesi"><i class="icon_genius"></i>Manage Sesi</a></li>
+      <li class=proper><a href="?manage_peserta"><i class="icon_genius"></i>Manage peserta</a></li>
+      <li class=proper><a href="../tmp/relasi.png" target="_blank"><i class="icon_genius"></i>Relasi DB</a></li>
 
 
       <!-- <li><a href="../"><i class="icon_house_alt"></i>Siakad Home</a></li> -->
